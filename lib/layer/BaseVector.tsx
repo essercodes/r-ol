a6eb7dcf9a8bdb02b3548900b0e9b2391f76a1e1
Max Esser <max@esser.codes>
2025-03-11 18:29:50 -0500

    created VectorTile.tsx and BaseVector.tsx

---
+++ lib/layer/BaseVector.tsx
@@ -0,0 +1,63 @@
+import {PropsWithChildren, useRef} from "react";
+
+import olBaseVectorLayer, {Options as olBaseVectorLayerOptions} from 'ol/layer/BaseVector';
+import {FeatureLike as olFeatureLike} from 'ol/Feature';
+import olVectorSource from 'ol/source/Vector';
+import olVectorTile from 'ol/source/VectorTile';
+import olCanvasVectorLayerRenderer from 'ol/renderer/canvas/VectorLayer'
+import olCanvasVectorTileLayerRenderer from 'ol/renderer/canvas/VectorTileLayer';
+import {StyleLike as olStyleLike} from "ol/style/Style";
+import {FlatStyleLike as olFlatStyleLike} from "ol/style/flat";
+
+import {Layer, LayerProps} from "./Layer"
+import {useSetProp} from "../UseSetProp.tsx";
+
+export type BaseVectorLayerProps<
+    FeatureType extends olFeatureLike,
+    VectorSourceType extends olVectorSource<FeatureType> | olVectorTile<FeatureType>,
+    RendererType extends olCanvasVectorLayerRenderer | olCanvasVectorTileLayerRenderer
+> = LayerProps & {
+    composing?: olBaseVectorLayer<FeatureType, VectorSourceType, RendererType>
+    initialOptions?: olBaseVectorLayerOptions<FeatureType, VectorSourceType>
+    declutter?: boolean,
+    style?: olStyleLike | olFlatStyleLike | null;
+}
+
+export function BaseVectorLayer<
+    FeatureType extends olFeatureLike,
+    VectorSourceType extends olVectorSource<FeatureType> | olVectorTile<FeatureType>,
+    RendererType extends olCanvasVectorLayerRenderer | olCanvasVectorTileLayerRenderer
+>(props: PropsWithChildren<BaseVectorLayerProps<FeatureType, VectorSourceType, RendererType>>) {
+    const baseVectorLayerRef = useRef<olBaseVectorLayer<FeatureType, VectorSourceType, RendererType> | null>(props.composing ?? null);
+
+    baseVectorLayerRef.current ??= new olBaseVectorLayer<FeatureType, VectorSourceType, RendererType>(props.initialOptions);
+
+    useSetProp(
+        baseVectorLayerRef,
+        props.declutter,
+        (baseVectorLayer: olBaseVectorLayer<FeatureType, VectorSourceType, RendererType>, value: boolean) => baseVectorLayer.setDeclutter(value),
+    )
+
+    useSetProp(
+        baseVectorLayerRef,
+        props.style,
+        (baseVectorLayer: olBaseVectorLayer<FeatureType, VectorSourceType, RendererType>, value: olStyleLike | olFlatStyleLike | null) => baseVectorLayer.setStyle(value),
+    )
+
+    return (
+        <Layer
+            composing={baseVectorLayerRef.current}
+            maxZoom={props.maxZoom}
+            opacity={props.opacity}
+            extent={props.extent}
+            maxResolution={props.maxResolution}
+            minResolution={props.minResolution}
+            minZoom={props.minZoom}
+            preload={props.preload}
+            visible={props.visible}
+            zIndex={props.zIndex}
+        >
+            {props.children}
+        </Layer>
+    )
+}
---
+++ lib/layer/VectorTile.tsx
@@ -0,0 +1,65 @@
+import {PropsWithChildren, useRef} from "react";
+
+import olVectorTileLayer, {Options as olVectorTileLayerOptions} from 'ol/layer/VectorTile.js';
+import olVectorTile from 'ol/source/VectorTile.js';
+import {FeatureLike as olFeatureLike} from "ol/Feature";
+import CanvasVectorTileLayerRenderer from "ol/renderer/canvas/VectorTileLayer";
+import {BackgroundColor as olBackgroundColor} from "ol/layer/Base";
+
+import {BaseVectorLayer, BaseVectorLayerProps} from "./BaseVector";
+import {useSetProp} from "../UseSetProp.tsx";
+
+export type VectorTileLayerProps<
+    VectorTileSourceType extends olVectorTile<FeatureType>,
+    FeatureType extends olFeatureLike
+> = BaseVectorLayerProps<FeatureType, VectorTileSourceType, CanvasVectorTileLayerRenderer> & {
+    composing?: olVectorTileLayer;
+    initialOptions?: olVectorTileLayerOptions;
+    background?: olBackgroundColor;
+    preload?: number;
+    useInterimTilesOnError?: boolean;
+}
+
+export function VectorTileLayer<
+    VectorTileSourceType extends olVectorTile<FeatureType>,
+    FeatureType extends olFeatureLike
+>(props: PropsWithChildren<VectorTileLayerProps<VectorTileSourceType, FeatureType>>) {
+    const vectorTileLayerRef = useRef<olVectorTileLayer | null>(props.composing ?? null);
+
+    vectorTileLayerRef.current ??= new olVectorTileLayer(props.initialOptions);
+
+    useSetProp(
+        vectorTileLayerRef,
+        props.background,
+        (vectorTileLayer: olVectorTileLayer, value: olBackgroundColor) => vectorTileLayer.setBackground(value),
+    )
+
+    useSetProp(
+        vectorTileLayerRef,
+        props.preload,
+        (vectorTileLayer: olVectorTileLayer, value: number) => vectorTileLayer.setPreload(value),
+    )
+
+    useSetProp(
+        vectorTileLayerRef,
+        props.useInterimTilesOnError,
+        (vectorTileLayer: olVectorTileLayer, value: boolean) => vectorTileLayer.setUseInterimTilesOnError(value),
+    )
+
+    return (
+        <BaseVectorLayer
+            composing={vectorTileLayerRef.current}
+            maxZoom={props.maxZoom}
+            opacity={props.opacity}
+            extent={props.extent}
+            maxResolution={props.maxResolution}
+            minResolution={props.minResolution}
+            minZoom={props.minZoom}
+            preload={props.preload}
+            visible={props.visible}
+            zIndex={props.zIndex}
+            declutter={props.declutter}
+            style={props.style}
+        ></BaseVectorLayer>
+    )
+}
