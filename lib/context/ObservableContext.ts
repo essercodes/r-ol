652f75fb0c783f14be13a8249004b39715a8cb12
Max Esser <max@esser.codes>
2025-03-17 13:44:44 -0500

    implemented useRevision with UseInstanceState

--- lib/context/ObservableContext.ts
+++ lib/context/ObservableContext.ts
@@ -1,13 +1,13 @@
 import {
-  createContext,
-  RefObject,
-  useContext,
-  useSyncExternalStore,
+    createContext,
+    RefObject,
+    useContext,
 } from "react";
 
 import olObservable from "ol/Observable";
 
-import { nullCheckContext } from "../Errors";
+import {nullCheckContext} from "../Errors";
+import {useInstanceState} from "../UseInstanceState.tsx";
 
 export type typeObservable = olObservable | null;
 export type typeObservableRef = RefObject<typeObservable>;
@@ -16,20 +16,16 @@ export type typeObservableContext = typeObservableRef | null;
 export const ObservableContext = createContext<typeObservableContext>(null);
 
 export function useObservable() {
-  return nullCheckContext(useContext(ObservableContext));
+    return nullCheckContext(useContext(ObservableContext));
 }
 
 export function useRevision() {
-  const instance = useObservable();
+    const instance = useObservable();
 
-  return useSyncExternalStore(
-    (callback) => {
-      instance.addEventListener("change", callback);
-
-      return () => {
-        instance.removeEventListener("change", callback);
-      };
-    },
-    () => instance.getRevision(),
-  );
+    return useInstanceState(
+        instance,
+        "change",
+        (instance: olObservable) => instance.getRevision.call(instance),
+        (instance: olObservable) => instance.changed.call(instance),
+    );
 }
