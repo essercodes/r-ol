57c5c6793922a166f18ebaa1b816f7c3952ef5d7
Max Esser <max@esser.codes>
2025-03-17 13:46:26 -0500

    added initialState to hooks

--- lib/context/BaseLayerContext.ts
+++ lib/context/BaseLayerContext.ts
@@ -1,10 +1,10 @@
-import { createContext, RefObject, useContext } from "react";
+import {createContext, RefObject, useContext} from "react";
 
 import olLayerBase from "ol/layer/Base";
-import { Extent as olExtent } from "ol/extent";
+import {Extent as olExtent} from "ol/extent";
 
-import { useInstanceState } from "../UseInstanceState";
-import { nullCheckContext } from "../Errors";
+import {useInstanceState} from "../UseInstanceState";
+import {nullCheckContext} from "../Errors";
 
 export type typeBaseLayer = olLayerBase | null;
 export type typeBaseLayerRef = RefObject<typeBaseLayer>;
@@ -13,101 +13,110 @@ export type typeBaseLayerContext = typeBaseLayerRef | null;
 export const BaseLayerContext = createContext<typeBaseLayerContext>(null);
 
 export function useBaseLayer() {
-  return nullCheckContext(useContext(BaseLayerContext));
+    return nullCheckContext(useContext(BaseLayerContext));
 }
 
 export type typeExtent = olExtent | undefined;
-export function useBaseLayerExtent() {
-  const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, typeExtent>(
-    layer,
-    "extent",
-    (layer: olLayerBase) => layer.getExtent.call(layer),
-    (layer: olLayerBase, extent: typeExtent) =>
-      layer.setExtent.call(layer, extent),
-  );
+export function useBaseLayerExtent(initialState?: typeExtent) {
+    const layer = useBaseLayer();
+
+    return useInstanceState<olLayerBase, typeExtent>(
+        layer,
+        "extent",
+        (layer: olLayerBase) => layer.getExtent.call(layer),
+        (layer: olLayerBase, extent: typeExtent) =>
+            layer.setExtent.call(layer, extent),
+        initialState,
+    );
 }
 
-export function useBaseLayerMaxResolution() {
-  const layer = useBaseLayer();
+export function useBaseLayerMaxResolution(initialState?: number) {
+    const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, number>(
-    layer,
-    "maxResolution",
-    (layer: olLayerBase) => layer.getMaxResolution.call(layer),
-    (layer: olLayerBase, maxResolution: number) =>
-      layer.setMaxResolution.call(layer, maxResolution),
-  );
+    return useInstanceState<olLayerBase, number>(
+        layer,
+        "maxResolution",
+        (layer: olLayerBase) => layer.getMaxResolution.call(layer),
+        (layer: olLayerBase, maxResolution: number) =>
+            layer.setMaxResolution.call(layer, maxResolution),
+        initialState,
+    );
 }
 
-export function useBaseLayerMaxZoom() {
-  const layer = useBaseLayer();
+export function useBaseLayerMaxZoom(initialState?: number) {
+    const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, number>(
-    layer,
-    "maxZoom",
-    (layer: olLayerBase) => layer.getMaxZoom.call(layer),
-    (layer: olLayerBase, maxZoom: number) =>
-      layer.setMaxZoom.call(layer, maxZoom),
-  );
+    return useInstanceState<olLayerBase, number>(
+        layer,
+        "maxZoom",
+        (layer: olLayerBase) => layer.getMaxZoom.call(layer),
+        (layer: olLayerBase, maxZoom: number) =>
+            layer.setMaxZoom.call(layer, maxZoom),
+        initialState,
+    );
 }
 
-export function useBaseLayerMinResolution() {
-  const layer = useBaseLayer();
+export function useBaseLayerMinResolution(initialState?: number) {
+    const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, number>(
-    layer,
-    "minResolution",
-    (layer: olLayerBase) => layer.getMinResolution.call(layer),
-    (layer: olLayerBase, minResolution: number) =>
-      layer.setMinResolution.call(layer, minResolution),
-  );
+    return useInstanceState<olLayerBase, number>(
+        layer,
+        "minResolution",
+        (layer: olLayerBase) => layer.getMinResolution.call(layer),
+        (layer: olLayerBase, minResolution: number) =>
+            layer.setMinResolution.call(layer, minResolution),
+        initialState,
+    );
 }
 
-export function useBaseLayerMinZoom() {
-  const layer = useBaseLayer();
+export function useBaseLayerMinZoom(initialState?: number) {
+    const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, number>(
-    layer,
-    "minZoom",
-    (layer: olLayerBase) => layer.getMinZoom.call(layer),
-    (layer: olLayerBase, minZoom: number) =>
-      layer.setMinZoom.call(layer, minZoom),
-  );
+    return useInstanceState<olLayerBase, number>(
+        layer,
+        "minZoom",
+        (layer: olLayerBase) => layer.getMinZoom.call(layer),
+        (layer: olLayerBase, minZoom: number) =>
+            layer.setMinZoom.call(layer, minZoom),
+        initialState,
+    );
 }
 
-export function useBaseLayerOpacity() {
-  const layer = useBaseLayer();
+export function useBaseLayerOpacity(initialState?: number) {
+    const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, number>(
-    layer,
-    "opacity",
-    (layer: olLayerBase) => layer.getOpacity.call(layer),
-    (layer: olLayerBase, opacity: number) =>
-      layer.setOpacity.call(layer, opacity),
-  );
+    return useInstanceState<olLayerBase, number>(
+        layer,
+        "opacity",
+        (layer: olLayerBase) => layer.getOpacity.call(layer),
+        (layer: olLayerBase, opacity: number) =>
+            layer.setOpacity.call(layer, opacity),
+        initialState,
+    );
 }
 
-export function useBaseLayerVisible() {
-  const layer = useBaseLayer();
+export function useBaseLayerVisible(initialState?: boolean) {
+    const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, boolean>(
-    layer,
-    "visible",
-    (layer: olLayerBase) => layer.getVisible.call(layer),
-    (layer: olLayerBase, visible: boolean) =>
-      layer.setVisible.call(layer, visible),
-  );
+    return useInstanceState<olLayerBase, boolean>(
+        layer,
+        "visible",
+        (layer: olLayerBase) => layer.getVisible.call(layer),
+        (layer: olLayerBase, visible: boolean) =>
+            layer.setVisible.call(layer, visible),
+        initialState,
+    );
 }
 
-export function useBaseLayerZIndex() {
-  const layer = useBaseLayer();
+export function useBaseLayerZIndex(initialState?: number) {
+    const layer = useBaseLayer();
 
-  return useInstanceState<olLayerBase, number | undefined, number>(
-    layer,
-    "zIndex",
-    (layer: olLayerBase) => layer.getZIndex.call(layer),
-    (layer: olLayerBase, zIndex: number) => layer.setZIndex.call(layer, zIndex),
-  );
+    return useInstanceState<olLayerBase, number | undefined, number>(
+        layer,
+        "zIndex",
+        (layer: olLayerBase) => layer.getZIndex.call(layer),
+        (layer: olLayerBase, zIndex: number) => layer.setZIndex.call(layer, zIndex),
+        initialState,
+    );
 }
